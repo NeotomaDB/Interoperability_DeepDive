847e1ee099edfcafdeb8bf2d37a22f334cbd7dd3
Simon <simon.j.goring@gmail.com>
2024-12-02 12:01:06 -0800

    Moved the `merged_records` file into the main looping script.
    Previously we had to match the resource post-hoc. This now includes DOI, snippet, article title and data resource name all in one loop.
    
    Also broke out the output files into more manageable chunks, resource by resource, to potentially help with discovery.

--- src/interop_dd.py
+++ src/interop_dd.py
@@ -3,14 +3,24 @@ import interoperability_deepdive as iod
 
 # This will generate a large-ish number of papers and grants.
 
-with open('../data/term_records.csv', 'r') as terms:
+with open('./data/term_records.csv', 'r') as terms:
     repositories = terms.read().splitlines()
 
-for i in repositories:
+dbs = []
+
+with open('./data/merged_records.csv', 'r', encoding='UTF-8') as terms:
+    reader = csv.reader(terms)
+    for i in reader:
+        dbs.append([j.strip() for j in i])
+
+for i in dbs:
     full_list = []
     print(i)
-    full_list = full_list + iod.gdd_snippets(i)
-    if len(full_list) > 0:
-        with open('../data/repohits.csv', 'a', encoding='utf-8', newline="") as file:
-            dictwriter = csv.DictWriter(file, full_list[0].keys())
-            dictwriter.writerows(full_list)
+    try:
+        full_list = full_list + iod.gdd_snippets(i[0])
+        if len(full_list) > 0:
+            with open(f'./data/{i[1]}.csv', 'a', encoding='utf-8', newline="") as file:
+                dictwriter = csv.DictWriter(file, full_list[0].keys())
+                dictwriter.writerows(full_list)
+    except Exception as e:
+        print(e)
